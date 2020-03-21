913ed2bbca2fe49fe872025befbb17d9c2dc350e
Alexander Stifel <VoteAlex@users.noreply.github.com>
2020-03-21 17:35:37 +0100

    Feature/landing page (#3)
    * Added hero to landing page
    
    * added undraw pictures
    
    * Added grid to landing page
    
    * Added cards to landing page
    
    * Optimized padding
    
    * Added Google Analytics and Performance
    
    * Added new image
    
    * Added missing analytics key
    
    * Feature/info page (#1)
    
    * Add Info header
    
    * added info page text with mat-card
    
    Co-authored-by: Dominik Klingert <dominik.klingert@mhp.com>
    Co-authored-by: Maurice K <mauricekopp21@gmail.com>
    
    * Fixed info conflicts
    
    * Added the team
    
    * added maurice team
    
    * Added footer
    
    * Added team image
    
    * Added add job form
    
    Co-authored-by: Alexander Stifel <Alexander.Stifel@mhp.com>
    Co-authored-by: Maurice K <mauricekopp21@gmail.com>
    Co-authored-by: Dominik Klingert <dominik.klingert@mhp.com>

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -4,12 +4,15 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatCardModule } from '@angular/material/card';
-
+import { MatInputModule } from '@angular/material/input';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 
 const modules = [
   MatToolbarModule,
   MatButtonModule,
   MatIconModule,
+  MatInputModule,
+  MatCheckboxModule,
   MatCardModule
 ];
 
