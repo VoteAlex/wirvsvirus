4ec9e06b8ab6cd4fd97e3fe8847b97a7f730f804
PKief <philipp.kief@gmx.de>
2020-03-21 23:12:37 +0100

    Add missing dependency

--- src/app/page/add-job/add-job.module.ts
+++ src/app/page/add-job/add-job.module.ts
@@ -1,9 +1,9 @@
 import { CommonModule } from '@angular/common';
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MaterialModule } from 'src/app/material/material.module';
 import { AddJobRoutingModule } from './add-job-routing.module';
 import { AddJobComponent } from './add-job.component';
-import { MaterialModule } from 'src/app/material/material.module';
 
 @NgModule({
   declarations: [AddJobComponent],
@@ -13,6 +13,9 @@ import { MaterialModule } from 'src/app/material/material.module';
     MaterialModule,
     ReactiveFormsModule,
     AddJobRoutingModule
+  ],
+  schemas: [
+    CUSTOM_ELEMENTS_SCHEMA
   ]
 })
 export class AddJobModule { }
