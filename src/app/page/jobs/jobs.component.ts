75d223e58bd5be32093eda15e3a2317004ee7158
PKief <philipp.kief@gmx.de>
2020-03-21 10:46:52 +0100

    Get data from firestore

--- src/app/page/jobs/jobs.component.ts
+++ src/app/page/jobs/jobs.component.ts
@@ -1,46 +1,48 @@
-import {
-  Component,
-  OnInit
-} from '@angular/core';
-import {
-  JobService
-} from 'src/app/services/job.service';
-import {
-  Job
-} from 'src/app/services/job.model';
-import {
-  GeolocationService
-} from 'src/app/services/geolocation.service';
-import { NumberSymbol } from '@angular/common';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { GeolocationService } from 'src/app/services/geolocation.service';
+import { Job } from 'src/app/services/job.model';
+import { JobService } from 'src/app/services/job.service';
+import { takeUntil } from 'rxjs/operators';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-jobs',
   templateUrl: './jobs.component.html',
   styleUrls: ['./jobs.component.css']
 })
-export class JobsComponent implements OnInit {
+export class JobsComponent implements OnInit, OnDestroy {
 
 
   jobs: Job[];
   latitude: number;
   longitude: number;
+  destroyed$ = new Subject();
 
-  constructor(public jobService: JobService, public geoService: GeolocationService) {}
+  constructor(public jobService: JobService, public geoService: GeolocationService) { }
 
-  async ngOnInit(): Promise < void > {
+  async ngOnInit(): Promise<void> {
 
     try {
 
-      const p  = await this.geoService.getCurrentPosition();
+      const p = await this.geoService.getCurrentPosition();
       this.latitude = p.coords.latitude;
       this.longitude = p.coords.longitude;
-      
-      this.jobs = await this.jobService.getJobs();
+
+      this.jobService.getJobs().pipe(
+        takeUntil(this.destroyed$),
+      ).subscribe(jobs => {
+        this.jobs = jobs;
+      })
 
     } catch (error) {
       console.error(error);
     }
 
+  }
+
+  ngOnDestroy() {
+    this.destroyed$.next();
+    this.destroyed$.complete();
   }
 
 }
--- src/app/services/job.service.ts
+++ src/app/services/job.service.ts
@@ -1,31 +1,18 @@
-import {
-  Injectable
-} from '@angular/core';
-import {
-  Job
-} from './job.model';
+import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
 import 'firebase/firestore';
-import {
-  AngularFirestore
-} from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+import { Job } from './job.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class JobService {
 
-  constructor(private firestore: AngularFirestore) {}
+  constructor(private firestore: AngularFirestore) { }
 
-  async getJobs(): Promise < Job[] > {
-
-    const j1 = < Job > {
-      description: "Test Description",
-      title: "Title",
-      uid: "asdasd"
-    }
-
-    return [j1, j1, j1];
-
+  getJobs(): Observable<Job[]> {
+    return this.firestore.collection<Job>('jobs').valueChanges();
   }
 
 
