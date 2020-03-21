b8925f64418a9c3a2ebd457c99cf99dc14408fd2
PKief <philipp.kief@gmx.de>
2020-03-21 13:20:22 +0100

    Improve styling

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 const modules = [
   MatToolbarModule,
   MatButtonModule,
+  MatIconModule,
 ];
 
 @NgModule({
