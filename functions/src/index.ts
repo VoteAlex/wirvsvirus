3503c462c1f031558bef5f44b312774930777914
Johannes Bitsch <johannes.bitsch@mhp.com>
2020-03-22 16:24:34 +0100

    add functions to automatically increment and decrement the job counter for new/deleted jobs

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,6 +3,28 @@ import * as functions from 'firebase-functions';
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 
-export const helloWorld = functions.https.onRequest((request, response) => {
-    response.send("Hello from Firebase!");
-});
+const admin = require('firebase-admin');
+admin.initializeApp();
+
+const db = admin.firestore();
+
+export const incrementJobCounter = functions.firestore
+  .document('jobs/{jobId}')
+  .onCreate(
+      (change, context) => {
+        console.log("create")
+        db.doc('/counters/lLcm2I58RLbRU7nOKc4h').update({"jobs": admin.firestore.FieldValue.increment(1)})
+
+        return 0;
+      }
+    );
+
+export const decrementJobCounter = functions.firestore
+    .document('jobs/{jobId}')
+    .onDelete(
+        (change, context) => {
+          db.doc('/counters/lLcm2I58RLbRU7nOKc4h').update({"jobs": admin.firestore.FieldValue.increment(-1)})
+
+          return 0;
+        }
+    );
